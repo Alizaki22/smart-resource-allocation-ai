ca3501ffc175b5223181795938a3b6181b493112
Alizaki22 <aliyu22mof@gmail.com>
2026-04-14 20:32:52 +0530

    config: move API URL to environment file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,1 @@
-const BASE_URL = "http://localhost:3000";
-
-export const predict = async (data) => {
-  const res = await fetch(`${BASE_URL}/predict`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  return res.json();
-};
+const BASE_URL = import.meta.env.VITE_API_URL;

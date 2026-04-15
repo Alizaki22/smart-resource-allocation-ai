e81f8eefe779e488e39046daf3afa22d73f1b806
ALIYU GAMBO ABUBAKAR <aliyu22mof@gmail.com>
2026-04-16 00:45:13 +0530

    Fix frontend API baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api", // backend
+  baseURL: "https://backend-service-w0jj.onrender.com/api",
 });
 
 // ✅ Single clean function

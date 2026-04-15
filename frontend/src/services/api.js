25c18150ef9578d708375a8b5ee22071f52fe945
ALIYU GAMBO ABUBAKAR <aliyu22mof@gmail.com>
2026-04-16 01:58:27 +0530

    Fix API route with /api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-service-w0jj.onrender.com",
+  baseURL: "https://backend-service-w0jj.onrender.com/api"
 });
 
 // ✅ Single clean function
